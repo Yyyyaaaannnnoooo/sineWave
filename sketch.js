ab0599db79d1a2b959f41419c42e045bc0b9fef3
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2018-04-16 20:55:52 +0200

    add offset points

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let cnv;
-let radius = 10;
+let radius = 20;
 let vertices = 4;
 let inc;
 let wave = 0;
@@ -10,30 +10,33 @@ let polygonNum = 30;
 function setup() {
   cnv = createCanvas(innerWidth, innerHeight);
   cnv.parent('p5Sketch');
-  inc = PI / 30;
+  inc = PI / 50;
 }
 
 function draw() {
   background(51);
   noFill();
+  strokeWeight(3)
   stroke(0, 255, 255);
-
-  let value = document.getElementById('vertices').value;
-  vertices = value;
+  // get the values from the HTML sliders
+  vertices = document.getElementById('vertices').value;
+  period = document.getElementById('frequency').value;
+  waveHeight = document.getElementById('height').value;
+  polygonNum = document.getElementById('number').value;
   for (let i = 1; i <= polygonNum; i++) {
     waveFreq = map(i, 0, polygonNum, 0, TWO_PI * period);
     let y = (height / 2) + (sin(wave + waveFreq) * waveHeight);
-    polygon(vertices, radius * i, width / 2, y);
+    polygon(vertices, radius * i, 5 * i, width / 2, y);
   }
   wave += inc;
 }
 
 // rect(x, y, 300, 500);
 
-function polygon(vert, r, posX, posY) {
-  beginShape();
+function polygon(vert, r, offset, posX, posY) {
+  beginShape(POINTS);
   for (let i = 0; i < vert; i++) {
-    let angle = map(i, 0, vert, 0, TWO_PI);
+    let angle = map(i, 0, vert, 0 + offset, TWO_PI + offset);
     let x = cos(angle) * r + posX;
     let y = sin(angle) * (r / 2) + posY;
     vertex(x, y);
