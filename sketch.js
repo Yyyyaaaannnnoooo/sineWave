46ddea4690d0bc2ef2828cadc385f17a537e293d
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2018-04-16 19:36:06 +0200

    Add polygon function

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 let cnv;
+let radius = 10;
+let vertices = 4;
 function setup() {
   cnv = createCanvas(innerWidth, innerHeight);
   cnv.parent('p5Sketch');
@@ -6,9 +8,29 @@ function setup() {
 
 function draw() {
   background(51);
-  // put drawing code here
+  noFill();
+  stroke(0, 255, 255);
+
+  let value = document.getElementById('vertices').value;
+  vertices = value;
+  for (let i = 1; i <= 10; i++) {
+    polygon(vertices, radius * i, width / 2, height / 2);
+  }
 }
 
-function windowResized(){
+// rect(x, y, 300, 500);
+
+function polygon(vert, r, posX, posY) {
+  beginShape();
+  for (let i = 0; i < vert; i++) {
+    let angle = map(i, 0, vert, 0, TWO_PI);
+    let x = cos(angle) * r + posX;
+    let y = sin(angle) * (r / 2) + posY;
+    vertex(x, y);
+  }
+  endShape(CLOSE);
+}
+
+function windowResized() {
   resizeCanvas(innerWidth, innerHeight);
 }
